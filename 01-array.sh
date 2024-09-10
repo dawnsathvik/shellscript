77e50de5326d03e0219023f374d5d524f8d72298
dawnsathvik <dawnsathvik678@gmail.com>
2024-09-10 10:01:46 +0530

    sv02

--- 01-array.sh
+++ 01-array.sh
@@ -3,3 +3,4 @@
 fruits=("apple" "cherry" "kewi")
 echo "first fruit: ${fruits[0]}"
 echo "all fruit: ${fruits[@]}"
+echo "first fruit: ${fruits[1]}"
---
+++ 02-specialvar.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "all var: $@"
+echo "no of var pasesed: $#"
+echo "script name: $0"
